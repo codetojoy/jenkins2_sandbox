1de06a74777a5bc8397d076115dfdaf95474f55b
Michael Easter <codetojoy@gmail.com>
2016-12-07 16:19:38 -0400

    incremental change as part of a larger frenzy of activity

--- resources/stage.sh
+++ resources/stage.sh
@@ -15,6 +15,7 @@ mkdir -p $DEST_DIR
 cp $SRC_DIR/greeting.war $DEST_DIR
 
 if [ "$ENV" == "DEV" ]; then
-    cp $SRC_DIR/greeting.war $DEST_ROOT/../servers/tomcat_DEV/data
+    mkdir -p $DEST_ROOT/../servers/tomcat_DEV/data
+    cp $SRC_DIR/greeting.war $DEST_ROOT/../servers/tomcat_DEV/data/greeting.war
 fi
 
