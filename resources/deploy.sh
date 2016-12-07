39ce5272732a0a9f544c38890181b44d28f8ece7
Michael Easter <codetojoy@gmail.com>
2016-12-07 15:41:07 -0400

    incremental change as part of a larger frenzy of activity

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -3,7 +3,17 @@
 ENV=$1
 BUILD_NUMBER=$2 
 
+SHARE_ROOT=/share
+WAR_FILE=${SHARE_ROOT}/${ENV}/${BUILD_NUMBER}/greeting.war
+
+stat $WAR_FILE
+
 echo "------------------------"
 echo "deploying war (build # $BUILD_NUMBER) to $ENV"
 echo "------------------------"
 
+if [ "$ENV" == "DEV" ]; then
+    # curl --user admin:devdevdev 'http://192.168.99.100:5555/manager/text/stop?path=/greeting'
+    # curl --user admin:devdevdev 'http://192.168.99.100:5555/manager/text/undeploy?path=/greeting'
+    curl --user admin:devdevdev 'http://192.168.99.100:5555/manager/text/deploy?path=/greeting&war=file:${WAR_FILE}'
+fi 
