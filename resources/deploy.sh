ec56f71f6c3fd94c3c19a6926660c47d5d52a195
Michael Easter <codetojoy@gmail.com>
2016-12-10 08:35:50 -0400

    various changes so that command-line list seems to work

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -8,7 +8,7 @@ echo "deploying war (build # $BUILD_NUMBER) to $ENV"
 echo "------------------------"
 
 if [ "$ENV" == "DEV" ]; then
-    # curl --user admin:devdevdev 'http://192.168.99.100:5555/manager/text/stop?path=/greeting'
-    # curl --user admin:devdevdev 'http://192.168.99.100:5555/manager/text/undeploy?path=/greeting'
+    curl --user admin:devdevdev 'http://192.168.99.100:5555/manager/text/stop?path=/greeting'
+    curl --user admin:devdevdev 'http://192.168.99.100:5555/manager/text/undeploy?path=/greeting'
     curl --user admin:devdevdev 'http://192.168.99.100:5555/manager/text/deploy?path=/greeting&war=file:/data/greeting.war'
 fi 
